e8a95755ea54d3cda7b406f3cf1b2792fce198d3
DesignerPH927 <designer.ph927@gmail.com>
2023-02-19 16:52:35 +0800

    deploy

---
+++ assets/AdminOrders-38baee0d.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-6eff3204.js";const n={};function o(t,s){return c(),r("div",null," 後台訂單列表 ")}const a=e(n,[["render",o]]);export{a as default};
---
+++ assets/HomeView-2c4fb476.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as r,a as o,F as l}from"./index-6eff3204.js";const c={methods:{loginPage(){this.$router.push("/login")}}},i=o("div",null,"這是首頁",-1);function u(p,e,_,d,g,t){return a(),r(l,null,[i,o("button",{class:"btn btn-primary",type:"button",onClick:e[0]||(e[0]=(...n)=>t.loginPage&&t.loginPage(...n))},"登入")],64)}const f=s(c,[["render",u]]);export{f as default};
